166b9421f2b940226a1e063bd143330a737dab88
Cristian Krone <cristian_krone@hotmail.com>
2022-09-12 23:25:34 -0300

    Feito a rota para buscar o pokmeon e as informações dos pokemons no componente de detalhes

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,7 @@
+import { PokeApiService } from './../../service/poke-api.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+
+  constructor(private activedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
+    this.pokemon;
+
   }
 
+  get pokemon() {
+    const id = this.activedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+    
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+         console.log(res);
+      }
+    );
+  }
 }
