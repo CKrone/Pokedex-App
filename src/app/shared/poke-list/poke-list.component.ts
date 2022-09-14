8ecc1fb3e77dab50bdceb99d4dca37fab7682296
Cristian Krone <cristian_krone@hotmail.com>
2022-09-14 10:01:28 -0300

    Colocado animação na imagem e no nome do pokemon, quando for visualizar as informações do pokemon específico

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -10,16 +10,20 @@ export class PokeListComponent implements OnInit {
 
   private setAllPokemons: any;
   public getAllPokemons: any;
+  public apiError: boolean = false;
 
   constructor(private pokeApiService: PokeApiService) { }
   
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
       res => {
-        this.setAllPokemons = res.results;
-        console.log(this.setAllPokemons);
-        this.getAllPokemons = this.setAllPokemons;
-       } );
+        this.setAllPokemons = res.results;        
+        this.getAllPokemons = this.setAllPokemons;        
+       },
+       error => {
+        this.apiError = true;
+       });
+        
   }
   public getSearch(value: string){
     const filter = this.setAllPokemons.filter((res: any) => {
